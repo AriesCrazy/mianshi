577d58a8c31eab4629b996494f633d764e5b050c
Aries <3358248521@qq.com>
2026-02-27 17:11:43 +0800

    refactor(app): 重命名主应用程序类
    - 将 MianshiApplication 重命名为 App
    - 更新 SpringApplication.run 方法中的类引用
    - 简化了类名以提高可读性

--- src/main/java/com/czy/App.java
+++ src/main/java/com/czy/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MianshiApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MianshiApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
